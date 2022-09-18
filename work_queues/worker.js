df533f92abe05a644f305f7206109a28b1cf1c5c
Imamov Erzhigit <erjigit17@gmail.com>
2022-09-19 05:03:21 +0600

    add Work Queues

---
+++ work_queues/worker.js
@@ -0,0 +1,32 @@
+const amqp = require('amqplib/callback_api');
+
+amqp.connect('amqp://localhost', (error0, connection) => {
+  if (error0) {
+    throw error0;
+  }
+
+  connection.createChannel((error1, channel) => {
+    if (error1) {
+      throw error1
+    }
+    const queue = 'task_queue'
+
+    // This makes sure the queue is declared before attempting to consume from it
+    channel.assertQueue(queue, {
+      durable: true
+    })
+
+    channel.consume(queue, msg => {
+      const secs = msg.content.toString().split('.').length - 1
+      console.log(` [x] Received ${msg.content.toString()}`)
+      console.log(secs)
+
+      setTimeout(()=> {
+        console.log(` [x] Done`)
+      }, secs * 1000)
+    }, {
+      // automatic acknowledgment mode,
+      noAck: true
+    });
+  });
+});
